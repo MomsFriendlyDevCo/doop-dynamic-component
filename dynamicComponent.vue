0446e08cb1b441dd2a1399aa40acb8f64106b24e
Mr-Yellow <mr-yellow@mr-yellow.com>
2022-11-16 09:10:34 +1000

    feat: Support for passing unbound attributes

--- dynamicComponent.vue
+++ dynamicComponent.vue
@@ -1,11 +1,11 @@
 <script lang="js" frontend>
-// TODO: This is dependancy of @doop/search but currently must be included at project level. it seems bigger than @doop/search and should exist in it's own package where both or either can require it.
 /**
 * Extended version of the built in `<script lang="js" frontend>` dynamic component that supports events and props and sanity checks
 *
-* @param {string} [component] The component name if passed a string (the component name is always camelCased so you can pass in dotted, kebab case of any other varient)
-* @param {Object} [events] Event bindings as an object lookup
+* @param {string} component The component name if passed a string (the component name is always camelCased so you can pass in dotted, kebab case of any other varient)
+* @param {Object} [attrs] Unbound attributes as an object lookup
 * @param {Object} [props] Prop bindings as an object lookup
+* @param {Object} [events] Event bindings as an object lookup
 *
 * NOTE: This component exposes a single ref `component` which is the dynamically loaded component
 */
@@ -26,6 +26,8 @@ app.component('dynamicComponent', {
 	render: function(h) {
 		return h(_.camelCase(this.$props.component), {
 			ref: 'component',
+			key: _.camelCase(this.$props.component),
+			attrs: this.attrs,
 			props: this.props,
 			on: this.events,
 		});
